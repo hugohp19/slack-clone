2eea24bca6fd03536a58a75e920263bdc1b80829
hugohp19 <hp19@hotmail.com>
2021-02-25 21:25:34 -0500

    project initialized and header and left channel container

---
+++ src/components/Header.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled from 'styled-components';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+  return (
+    <Container>
+      <Main>
+        <AccessTimeIcon />
+        <SearchContainer>
+          <Search>
+            <input type='text' placeholder='Search...'/>
+          </Search>
+        </SearchContainer>
+        <HelpOutlineIcon />
+      </Main>
+      <UserContainer>
+        <Name>
+          Hugo
+        </Name>
+        <UserImage>
+          <img src='https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg' />
+        </UserImage>
+      </UserContainer>
+    </Container>
+  )
+}
+
+export default Header;
+
+const Container = styled.div`
+  background: #350d36;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  box-shadow: inset 0 0 0 1px rgb(104 74 104);
+`
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 16px;
+  margin-right: 16px;
+`
+
+const SearchContainer = styled.div`
+  min-width: 400px;
+  margin-left: 16px;
+  margin-right: 16px;
+`
+
+const Search = styled.div`
+  box-shadow: inset 0 0 0 1px rgb(104 74 104);
+  border-radius: 6px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  input{
+    background-color: transparent;
+    border: none;
+    padding: 4px 8px;
+    width: 100%;
+    color: white;
+    outline: none;
+  }
+`
+
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 16px;
+  position: absolute;
+  right: 0;
+`
+
+const Name = styled.div`
+  padding-right: 16px;
+`
+
+const UserImage = styled.div`
+  width: 28px;
+  height: 28px;
+
+  img{
+    height: 100%;
+  }
+`
